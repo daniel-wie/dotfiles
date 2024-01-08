88605c5355b4de799a53147ae59699689e3df0ed
Daniel <daniel@mint21.ifp.tuwien.ac.at>
2024-01-08 11:52:43 +0100

    add telescope and config colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,14 @@ return {
 	priority = 1000,
 
 	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
+		require("catppuccin").setup({
+			flavour = "mocha",
+			dim_inactive = {
+				enabled = true, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
 	end
 }
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,40 @@
+-- https://github.com/nvim-telescope/telescope.nvim
+
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "TELESCOPE Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "TELESCOPE Live Grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "TELESCOPE List Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "TELESCOPE Find Help" },
+      { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "TELESCOPE Find Keymaps" },
+      { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "TELESCOPE Find Old Files" },
+    },
+    opts = {
+      defaults = {
+        layout_config = { prompt_position = "top" },
+        prompt_prefix = "> ",
+        selection_caret = "> ",
+        sorting_strategy = "ascending",
+      },
+      pickers = {
+        find_files = {
+          find_command = { "fdfind", "--type", "f", "--follow", "--hidden" },
+        },
+      },
+    },
+  },
+}
