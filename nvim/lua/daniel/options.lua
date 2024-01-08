672da3c4af6c245b5fe979504f6e2a8613c3c47f
Daniel <daniel@mint21.ifp.tuwien.ac.at>
2024-01-08 16:49:21 +0100

    add some options

--- nvim/lua/daniel/options.lua
+++ nvim/lua/daniel/options.lua
@@ -6,12 +6,15 @@ vim.g.maplocalleader = " "
 vim.opt.mouse = ""				-- Disable mouse
 vim.opt.number = true			-- Print line number
 vim.opt.relativenumber = true	-- Show relative line number
+vim.opt.cursorline = true		-- Highlight current line
 vim.opt.scrolloff = 8 			-- Minimal number of lines to keep around cursor
 vim.opt.shiftwidth = 4 			-- Number of spaces for indent
 vim.opt.smartindent = true 		-- Smart autoindent when starting a new line
 vim.opt.tabstop = 4 			-- Number of spaces <Tab> counts for
 vim.opt.wrap = true 			-- Line wrap
-vim.opt.termguicolors = true	-- true color terminal
+vim.opt.termguicolors = true	-- True color terminal
+vim.opt.undofile = true			-- Save undo history
+vim.opt.clipboard = "unnamedplus"	-- Use system clipboard
 
 -- Julia
 vim.cmd("let g:latex_to_unicode_keymap = 1")	-- In-place replacement of unicode symbols in Julia
