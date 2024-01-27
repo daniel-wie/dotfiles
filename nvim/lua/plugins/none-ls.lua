ee263054114b726c66d45d84a2516d71c1edc8e4
Daniel Wieser <dan.wie.12@gmail.com>
2024-01-27 18:18:29 +0100

    Add formatter and autoformatting.

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,13 @@
+return {
+	"nvimtools/none-ls.nvim",
+
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua, -- lua formatting
+				null_ls.builtins.code_actions.gitsigns, -- gitsigns in code actions
+			},
+		})
+	end,
+}
