51108adf6f903a79defeaa640207365047618c97
Daniel Wieser <dan.wie.12@gmail.com>
2024-05-16 23:49:36 +0200

    Remove stylua.

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,7 +6,6 @@ return {
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua, -- lua formatting
 				null_ls.builtins.code_actions.gitsigns, -- gitsigns in code actions
 			},
 		})
