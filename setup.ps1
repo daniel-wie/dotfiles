4757a24e202ad05f442893047fc2a1200abacb1c
Daniel Wieser <dan.wie.12@gmail.com>
2024-09-26 17:19:45 +0200

    Add `.julia/config` to `setup.ps1`.

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
 # Link AppData/Local/nvim to nvim
 New-Item -Path C:\Users\balld\AppData\Local\nvim -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.config\nvim
 New-Item -Path C:\Users\balld\AppData\Roaming\fd -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.config\fd
+New-Item -Path C:\Users\balld\.julia\config -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.julia\config
