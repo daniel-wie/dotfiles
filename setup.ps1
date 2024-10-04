c69bd6dc97c37cdf60518280bee34031c62530fb
Daniel Wieser <dan.wie.12@gmail.com>
2024-10-04 18:00:50 +0200

    Make `markdown-preview.nvim` and `prettierd` work on Windows.

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,6 @@
-# Link AppData/Local/nvim to nvim
+# This script does the same as gnu stow but by hand...
 New-Item -Path C:\Users\balld\AppData\Local\nvim -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.config\nvim
 New-Item -Path C:\Users\balld\AppData\Roaming\fd -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.config\fd
 New-Item -Path C:\Users\balld\.julia\config -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.julia\config
+New-Item -Path C:\Users\balld\.prettierrc -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\.prettierrc
+New-Item -Path D:\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Target C:\Users\balld\.dotfiles\PowerShell_profile.ps1
