9eab12cbcfc0e5092f5221c117ab4aa6d442134b
Daniel Wieser <dan.wie.12@gmail.com>
2024-10-11 10:35:07 +0200

    Add `.julia/environments` and `.gitignore`.

--- setup.ps1
+++ setup.ps1
@@ -12,6 +12,7 @@ function New-SymbolicLink {
 New-SymbolicLink $HOME\AppData\Local\nvim $HOME\.dotfiles\.config\nvim
 New-SymbolicLink $HOME\AppData\Roaming\fd $HOME\.dotfiles\.config\fd
 New-SymbolicLink $HOME\.julia\config $HOME\.dotfiles\.julia\config
+New-SymbolicLink $HOME\.julia\environments $HOME\.dotfiles\.julia\environments
 New-SymbolicLink $HOME\.prettierrc $HOME\.dotfiles\.prettierrc
 New-SymbolicLink $HOME\.ssh\config $HOME\.dotfiles\.ssh\config
 New-SymbolicLink D:\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 $HOME\.dotfiles\PowerShell_profile.ps1
