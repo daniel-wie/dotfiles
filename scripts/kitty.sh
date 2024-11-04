b2a67c22c90d8248bf64caacce68d717c812d2b6
Daniel Wieser <dan.wie.12@gmail.com>
2024-11-04 12:38:18 +0100

    Add `scripts` and `kitty.sh`.

---
+++ scripts/kitty.sh
@@ -0,0 +1,14 @@
+# Installation according to
+# https://sw.kovidgoyal.net/kitty/binary/
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# create symlinks
+cd $HOME/.local/kitty.app/
+stow -v .
+
+# make kitty the standard terminal for gnome
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $HOME/.local/bin/kitty
+
+# set theme
+kitten themes --reload-in=all Catppuccin-Mocha
