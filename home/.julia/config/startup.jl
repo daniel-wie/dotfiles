3196e8d3c5948227bdf8f9794de6fca39a909b17
Daniel Wieser <daniel.wieser@tuwien.ac.at>
2024-11-14 08:25:14 +0100

    Add automatic package loading in Julia.

--- home/.julia/config/startup.jl
+++ home/.julia/config/startup.jl
@@ -1,1 +1,16 @@
 ENV["JULIA_PKG_USE_CLI_GIT"] = true
+
+if isinteractive()
+    # Start Revise automatically
+    try
+        using Revise
+    catch e
+        @warn "Error initializing Revise" exception = (e, catch_backtrace())
+    end
+
+    # Activate current environment if existing
+    if isfile("Project.toml")
+        using Pkg
+        Pkg.activate(".")
+    end
+end
