077e1784a9bd61da8f5f542a8b71e7271d9e493a
Daniel Wieser <dan.wie.12@gmail.com>
2025-04-03 15:59:56 +0200

    Add `chroot.sh`.

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -3,21 +3,33 @@
 # https://github.com/frankebel/archinstall and
 # https://wiki.archlinux.org/title/Installation_guide.
 
-# Check UEFI 64-bit boot mode.
-[ $(< /sys/firmware/efi/fw_platform_size) == 64 ] || exit
-
 # Uncomment lines below and set values manually.
 # drive=              # Drive to install Arch Linux. (default: first drive)
 # swap_size=          # Swap size in GiB. Set 0 for none. (default: 0)
+# pass_root=          # Passphrase for root user. (default: pass)
+# username=           # Username for regular user. (default: user)
+# pass_user=          # Passphrase for regular user. (default: pass)
+# hostname=           # hostname of the device. (default: arch)
+# keymap=             # Keyboard mapping for console. (default: de-latin1)
 
 # Set default values.
 drive_default=$(lsblk -dno NAME | grep -E '^nvme|^sd|^vd' | head -n 1)
-drive=${drive:-$drive_default}
-swap_size=${swap_size:-0}
+: ${drive:=$drive_default}
+: ${swap_size:=0}
+: ${pass_root:=pass}
+: ${username:=user}
+: ${pass_user:=pass}
+: ${hostname:=arch}
+: ${keymap:=de-latin1}
 
-# Partition disks.
-# 1. Create 1024MB partition for EFI system.
-# 2. Create root partition with remaining capacity.
+# 1 Pre-installation
+
+# 1.6 Verify UEFI 64-bit boot mode.
+[ $(< /sys/firmware/efi/fw_platform_size) == 64 ] || exit
+
+# 1.9 Partition the disks
+# Create 1024MB partition for EFI system.
+# Create root partition with remaining capacity.
 fdisk /dev/$drive << FDISK_CMDS
 g
 n
@@ -36,18 +48,52 @@ FDISK_CMDS
 efi_system_partition=$(lsblk -rno NAME /dev/$drive | grep $drive.*1)
 root_partition=$(lsblk -rno NAME /dev/$drive | grep $drive.*2)
 
-# Format partitions.
+# 1.10 Format the partitions
 mkfs.fat -F 32 /dev/$efi_system_partition
 mkfs.btrfs /dev/$root_partition
 
-# Mount filesystems.
+# 1.11 Mount the filesystems
 mount /dev/$root_partition /mnt
 mount --mkdir /dev/$efi_system_partition /mnt/boot
 
-# Create swapfile.
+# Create swapfile
+# https://wiki.archlinux.org/title/Btrfs#Swap_file
 if (( swap_size > 0 )); then
-    dd if=/dev/zero of=/mnt/swapfile bs=1GiB count=$swap_size
-    chmod 600 /mnt/swapfile
-    mkswap /mnt/swapfile
-    swapon /mnt/swapfile
+	btrfs subvolume create /mnt/swap
+	btrfs filesystem mkswapfile --size "${swap_size}g" --uuid clear /mnt/swap/swapfile
+	swapon /mnt/swap/swapfile
 fi
+
+# 2 Installation
+
+# 2.2 Install essential packages
+pacstrap -K /mnt base linux linux-firmware efibootmgr networkmanager neovim
+
+# 3 Configure the system
+
+# 3.1 Generate fstab file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# 3.2 Change root into the new system
+# Insert values into chroot.sh 
+uuid_root="$(lsblk -dno UUID "/dev/$root_partition")"
+
+sed -i -E "s/(^drive=$)/\1'$drive'/" chroot.sh
+sed -i -E "s/(^uuid_root=$)/\1'$uuid_root'/" chroot.sh
+sed -i -E "s/(^pass_root=$)/\1'$pass_root'/" chroot.sh
+sed -i -E "s/(^username=$)/\1'$username'/" chroot.sh
+sed -i -E "s/(^pass_user=$)/\1'$pass_user'/" chroot.sh
+sed -i -E "s/(^hostname=$)/\1'$hostname'/" chroot.sh
+sed -i -E "s/(^keymap=$)/\1'$keymap'/" chroot.sh
+
+cp chroot.sh /mnt/root/chroot.sh
+arch-chroot /mnt /root/chroot.sh
+shred -u /mnt/root/chroot.sh
+
+# 4 Reboot
+
+# Turn off swap and unmount all partitions.
+(( swap_size > 0 )) && swapoff /mnt/swap/swapfile
+umount -R /mnt
+
+printf '\033[1mInstallation is done, please reboot.\n'
---
+++ scripts/chroot.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Script to run inside chroot environment.
+
+# Values are set by "arch.sh". Do not edit yourself!
+drive=
+uuid_root=
+pass_root=
+username=
+pass_user=
+hostname=
+keymap=
+
+# 3.3 Time
+ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime
+hwclock --systohc
+
+# 3.4 Localization
+sed -i -E 's/^#(en_US.UTF-8 UTF-8\s*$)/\1/' /etc/locale.gen
+sed -i -E 's/^#(de_AT.UTF-8 UTF-8\s*$)/\1/' /etc/locale.gen
+locale-gen
+printf 'LANG=en_US.UTF-8\n' > /etc/locale.conf
+printf 'KEYMAP=%s\n' $keymap > /etc/vconsole.conf
+
+# 3.5 Network configuration
+printf '%s\n' $hostname > /etc/hostname
+systemctl enable NetworkManager.service
+
+# 3.7 Root password
+printf 'root:%s' $pass_root | chpasswd
+
+# Add user
+useradd -m -G wheel $username
+printf '%s:%s' $username $pass_user | chpasswd
+sed -i -E 's/^#\s*(%wheel ALL=\(ALL:ALL\) ALL$)/\1/' /etc/sudoers
+
+# 3.8 Boot loader
+# Install microcode updates, cf.
+# https://wiki.archlinux.org/title/Installation_guide#Install_essential_packages
+case $(lscpu | grep 'Vendor ID') in
+    *AuthenticAMD*)
+        microcode=amd-ucode
+        ;;
+    *GenuineIntel*)
+        microcode=intel-ucode
+        ;;
+    *)
+        printf 'Could not find microcode for processor. Aborting script.\n'
+        exit 1
+        ;;
+esac
+
+pacman -S --noconfirm $microcode
+
+# https://wiki.archlinux.org/title/EFISTUB#efibootmgr
+efibootmgr \
+    --create \
+    --disk /dev/$drive \
+    --part 1 \
+    --label "Arch Linux" \
+    --loader /vmlinuz-linux \
+    --unicode "root=UUID=$uuid_root rw initrd=\initramfs-linux.img quiet"
