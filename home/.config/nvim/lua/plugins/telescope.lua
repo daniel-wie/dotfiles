96f44eb37ab0c65a9258dc5fe08c433fff193455
Daniel Wieser <dan.wie.12@gmail.com>
2025-04-07 12:25:43 +0200

    Fix telescope for Arch.

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 local function find_files()
-	if vim.fn.has("win32") == 1 then
+	if vim.fn.has("win32") == 1 or string.find(vim.loop.os_uname().release, "arch") ~= nil then
 		return { find_command = { "fd", "--follow", "--hidden" } }
 	else
 		return { find_command = { "fdfind", "--follow", "--hidden" } }
@@ -25,11 +25,11 @@ return {
 		cmd = "Telescope",
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "TELESCOPE Find Files" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "TELESCOPE Live Grep" },
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "TELESCOPE List Buffers" },
-			{ "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "TELESCOPE Find Help" },
-			{ "<leader>fk", "<cmd>Telescope keymaps<cr>",    desc = "TELESCOPE Find Keymaps" },
-			{ "<leader>fo", "<cmd>Telescope oldfiles<cr>",   desc = "TELESCOPE Find Old Files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "TELESCOPE Live Grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "TELESCOPE List Buffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "TELESCOPE Find Help" },
+			{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "TELESCOPE Find Keymaps" },
+			{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "TELESCOPE Find Old Files" },
 		},
 		opts = {
 			defaults = {
