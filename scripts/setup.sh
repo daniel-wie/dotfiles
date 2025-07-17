92bce4a0a73e2bff16eea6094502f876f9fe9d54
Daniel Wieser <daniel.wieser@ait.ac.at>
2025-07-17 13:19:16 +0200

    Add `julia.sh`.

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# julia
-curl -fsSL https://install.julialang.org | sh 
-
-# installs nvm (Node Version Manager)
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-# nvm install node
-# npm install yarn
