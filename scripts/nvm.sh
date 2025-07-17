2891885e66374eee34f206df1116224587b1eab0
Daniel Wieser <daniel.wieser@ait.ac.at>
2025-07-17 13:13:52 +0200

    Add nvm and prettierd install scripts.

---
+++ scripts/nvm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
---
+++ scripts/prettierd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# https://github.com/fsouza/prettierd?tab=readme-ov-file#installation-guide
+npm install -g @fsouza/prettierd
