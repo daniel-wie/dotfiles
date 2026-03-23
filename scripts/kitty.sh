b8428652fc4d7cf7891f9e89fede20fff816ca5b
Daniel Wieser <daniel.wieser@ait.ac.at>
2026-03-23 22:32:07 +0100

    Add kitty-scrollback.nvim.

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -7,8 +7,5 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 cd $HOME/.local/kitty.app/
 stow -v .
 
-# make kitty the standard terminal for gnome
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $HOME/.local/bin/kitty 50
-
 # set theme
 # kitten themes --reload-in=all Catppuccin-Mocha
