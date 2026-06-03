cecb2024d635fe02a8c7ea82cfe98794fec93d69
Daniel Wieser <daniel.wieser@ait.ac.at>
2026-06-03 22:03:41 +0200

    fix(nvim): load ltex_extra earlier

--- home/.config/nvim/plugin/40_lsp.lua
+++ home/.config/nvim/plugin/40_lsp.lua
@@ -5,6 +5,12 @@ vim.pack.add({
 	},
 })
 
+-- https://github.com/barreiroleo/ltex_extra.nvim
+require("ltex_extra").setup({
+	load_langs = { "en-US", "de-AT" },
+	path = ".ltex",
+})
+
 vim.lsp.enable({
 	-- Julia
 	"jetls",
